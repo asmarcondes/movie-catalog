01b795958bcbdec0de15919dcf03ed4fb97e4ff7
Paulo Ricardo <paulopentest@gmail.com>
2022-11-20 00:58:39 +0000

    cria a estrutura para atores(scaffold)

---
+++ db/migrate/20221120005448_create_actors.rb
@@ -0,0 +1,12 @@
+class CreateActors < ActiveRecord::Migration[7.0]
+  def change
+    create_table :actors do |t|
+      t.string :name
+      t.integer :birth_year
+      t.string :email
+
+      t.timestamps
+    end
+    add_index :actors, :email, unique: true
+  end
+end
