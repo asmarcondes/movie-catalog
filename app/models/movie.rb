b993c4930bc5ec9ed11ef2dc4f95fa7ada5d5f9c
Anderson Marcondes <anderson@marcond.es>
2022-11-20 19:44:22 +0000

    define validações para model Movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,11 @@
 class Movie < ApplicationRecord
+  validates :title, :launch_year, :actors, presence: true
+  validates :title, uniqueness: true
+  validates :launch_year, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1888
+  }
+
   has_and_belongs_to_many :actors
-  has_many :review
+  has_many :review, dependent: :destroy
 end
