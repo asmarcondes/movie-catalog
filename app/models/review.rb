d5888188664f982ebe3e3ec6a6a918def4f4b114
Anderson Marcondes <anderson@marcond.es>
2022-11-21 04:40:30 +0000

    adiciona validação para a nota da avaliação

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,8 @@ class Review < ApplicationRecord
   belongs_to :movie
   scope :waiting_approval, -> { where(approved: false) }
   validates :movie, :comment, presence: true
+  validates :rating, numericality: {
+    only_integer: true,
+    in: 0..10
+  }
 end
