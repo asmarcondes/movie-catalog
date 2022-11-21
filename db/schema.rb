f2885b358167704db875d5b97f2336b471258151
Anderson Marcondes <anderson@marcond.es>
2022-11-21 01:04:20 +0000

    adiciona campo "approved" em reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_20_171856) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_004246) do
   create_table "actors", force: :cascade do |t|
     t.string "name"
     t.integer "birth_year"
@@ -35,9 +35,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_20_171856) do
   create_table "reviews", force: :cascade do |t|
     t.text "comment"
     t.integer "rating"
-    t.integer "movie_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "movie_id", null: false
+    t.boolean "approved", default: false
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
   end
 
