f0f1e8b42d472a951ded77f3fc376af9c097d006
Klecia Farias <kel.farias@hotmail.com>
2022-11-21 01:34:37 +0000

    valida(comments/movie) no modelo review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,5 @@ class Review < ApplicationRecord
   belongs_to :movie
   has_many :comments
 
-  validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 10 }
+  validates :movie, :comments, presence: true
 end
