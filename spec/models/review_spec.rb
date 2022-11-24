d3f978f6b7686409f7a9d677d82907ae47d7bda5
Klecia Farias <kel.farias@hotmail.com>
2022-11-24 14:57:58 +0000

    adiciona cases de testes do model review

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,8 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  it 'é válido com texto e filme associado'
-  it 'não é válido sem texto'
-  it 'não é válido sem filme associado'
-  it 'deve ser registrado sem estar aprovado'
+  it 'é válido com texto e filme associado' do
+    review = Review.new
+    review.comment = 'Primeiro comentario'
+    review.movie = 'Movie 1'
+
+    expect(review).to be_valid  
+  end
+  
 end
+
+  it 'não é válido sem texto' do
+    review = Review.new(comment: nil)
+    review.movie = 'Movie 1'
+
+    expect(review).to_not be_valid
+  end
+
+
+  it 'não é válido sem filme associado' do
+    review = Review.new(movie: nil)
+    review.comment = 'Primeiro comentario'
+
+    expect(review).to_not be_valid
+  end
+
+
+  it 'deve ser registrado sem estar aprovado' do
+    review = Review.new(comment.approved: false)
+    review.comment = 'Primeiro comentario'
+    review.movie = 'Movie 1'
+
+    expect(review).to be_valid
+  end
