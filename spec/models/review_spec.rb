c0e5181d85627268bd3bf68c29b4a34a6f2ad712
Anderson Marcondes <anderson@marcond.es>
2022-11-24 23:03:09 +0000

    refactor(test): ajusta algumas linhas do teste

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,22 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Review, type: :model do
   it 'é válido com texto e filme associado' do
+    movie = Movie.new(title: 'Movie 1', launch_year: 1980,
+                      actors: [Actor.new(name: 'Ator 1', email: 'ator1@teste.com', birth_year: 1980)])
+
     review = Review.new
     review.comment = 'Primeiro comentario'
     review.rating = 0
 
-    movie = Movie.new(title: 'Movie 1', launch_year: 1980,
-                      actors: [Actor.new(name: 'Ator 1', email: 'ator1@teste.com', birth_year: 1980)])
     review.movie = movie
 
     expect(review).to be_valid
   end
 
   it 'não é válido sem texto' do
-    review = Review.new(comment: nil)
-    review.rating = 0
     movie = Movie.new(title: 'Movie 1', launch_year: 1980,
                       actors: [Actor.new(name: 'Ator 1', email: 'ator1@teste.com', birth_year: 1980)])
+
+    review = Review.new(comment: nil)
+    review.rating = 0
     review.movie = movie
 
     expect(review).to_not be_valid
@@ -32,27 +34,27 @@ RSpec.describe Review, type: :model do
   end
 
   it 'deve ser registrado sem estar aprovado' do
-    review = Review.new(approved: false)
-    review.rating = 0
-    review.comment = 'Primeiro comentario'
-
     movie = Movie.new(title: 'Movie 1', launch_year: 1980,
                       actors: [Actor.new(name: 'Ator 1', email: 'ator1@teste.com', birth_year: 1980)])
+
+    review = Review.new(approved: false)
     review.movie = movie
+    review.rating = 0
+    review.comment = 'Primeiro comentario'
 
     expect(review).to be_valid
   end
 
   it 'deve registrar um log ao aprovar comentário' do
+    movie = Movie.new(title: 'Movie 1', launch_year: 1980,
+                      actors: [Actor.new(name: 'Ator 1', email: 'ator1@teste.com', birth_year: 1980)])
+
     review = Review.new(approved: false)
+    review.movie = movie
     review.rating = 0
     review.comment = 'Primeiro comentario'
 
-    movie = Movie.new(title: 'Movie 1', launch_year: 1980,
-                      actors: [Actor.new(name: 'Ator 1', email: 'ator1@teste.com', birth_year: 1980)])
-    review.movie = movie
     review.save
-
     review.update({ approved: true })
 
     log = Log.find_by(review_id: review.id)
