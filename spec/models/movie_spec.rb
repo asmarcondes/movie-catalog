bb8a09d90582c314bc070d50d01bf78dcd3d445c
Anderson Marcondes <anderson@marcond.es>
2022-11-24 21:21:21 +0000

    teste: utiliza novos recursos para os testes

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,66 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  subject do
-    described_class.new(
-      title: 'Título 1',
-      launch_year: 1980,
-      actors: [Actor.new(name: 'Ator 1', email: 'ator1@x.com', birth_year: 1980)]
-    )
+  subject(:movie) { described_class.new(title:, launch_year:, actors:) }
+
+  let(:title) { 'Título 1' }
+  let(:launch_year) { 1980 }
+  let(:actors) { [Actor.new(name: 'Ator 1', email: 'ator1@teste.com', birth_year: 1980)] }
+
+  describe '.new' do
+    context 'com atributos válidos' do
+      it { is_expected.to be_a(described_class) }
+      it { is_expected.to be_valid }
+    end
   end
 
-  it 'é válido com título, ano e elenco' do
-    expect(subject).to be_valid
-  end
+  describe '#valid?' do
+    context 'com título inválido' do
+      [nil].each do |title|
+        context "quando título é #{title.inspect}" do
+          let(:title) { title }
 
-  it 'não é válido sem um título' do
-    subject.title = nil
+          it { is_expected.to be_a(described_class) }
+          it { is_expected.to be_invalid }
+        end
+      end
+    end
 
-    expect(subject).to_not be_valid
-  end
+    context 'com título duplicado' do
+      it 'não é válido' do
+        subject.save
 
-  it 'não é válido se outro filme possuir mesmo título' do
-    subject.save
+        movie2 = Movie.new
+        movie2.title = subject.title
+        movie2.launch_year = 1980
+        movie2.actors << Actor.new(name: 'Ator 1', email: 'ator1@teste.com', birth_year: 1980)
+        movie2.save
 
-    movie2 = Movie.new
-    movie2.title = subject.title
-    movie2.launch_year = 1980
-    movie2.actors << Actor.new(name: 'Ator 1', email: 'ator1@x.com', birth_year: 1980)
-    movie2.save
+        expect(movie2).to_not be_valid
+      end
+    end
 
-    expect(movie2).to_not be_valid
-  end
+    context 'com ano inválido' do
+      [nil, 1887, [], true, 'banana'].each do |launch_year|
+        context "quando o ano é #{launch_year.inspect}" do
+          let(:launch_year) { launch_year }
+          it { is_expected.to be_invalid }
+        end
+      end
+    end
 
-  it 'não é válido sem ano de lançamento' do
-    subject.launch_year = nil
-
-    expect(subject).to_not be_valid
-  end
-
-  it 'não é válido com ano de lançamento menor do que 1888' do
-    subject.launch_year = 1887
-
-    expect(subject).to_not be_valid
-  end
-
-  it 'não é válido com ano de lançamento sem número válido' do
-    # Movie#launch_year é definido como 0
-    # movie = Movie.new(launch_year: 'xxx')
-    # movie = Movie.new(launch_year: false)
-
-    subject.launch_year = []
-    expect(subject).to_not be_valid
-
-    subject.launch_year = true
-    expect(subject).to_not be_valid
-
-    subject.launch_year = 'banana'
-    expect(subject).to_not be_valid
-  end
-
-  it 'não é válido sem elenco de atores' do
-    subject.actors.clear
-
-    expect(subject).to_not be_valid
+    context 'sem elenco' do
+      let(:actors) { [] }
+      it { is_expected.to be_invalid }
+    end
   end
 end
