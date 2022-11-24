240aa9261a12f57d51e5be63eb28c656c0d9e5db
Anderson Marcondes <anderson@marcond.es>
2022-11-24 22:09:02 +0000

    feat: valida email e data de nascimento

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,5 +1,13 @@
+require 'uri'
+
 class Actor < ApplicationRecord
   has_and_belongs_to_many :movies
+
   validates :name, :birth_year, :email, presence: true
   validates :email, uniqueness: true
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+  validates :birth_year, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1700
+  }
 end
