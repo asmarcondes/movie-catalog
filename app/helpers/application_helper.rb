e021b22212853a2e8923aa5a9c4aa4a758838cbf
Anderson Marcondes <anderson@marcond.es>
2022-11-26 00:22:06 +0000

    style: mudança no layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  PAGES = {
+    '/movies' => 'Movies',
+    '/actors' => 'Actors',
+    '/reviews' => 'Reviews',
+    '/logs' => 'Logs'
+  }.freeze
+
+  def active_class(path)
+    return 'bg-gray-900 text-white' if request.path == path
+
+    'text-gray-300 hover:bg-gray-700 hover:text-white'
+  end
+
+  def active_page
+    PAGES[request.path]
+  end
 end
